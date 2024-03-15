7dd02288cce917847f2f6366727767ad6c748271
Liv Asch <olivia.c.asch@gmail.com>
2024-03-15 19:26:31 +0000

    Add script for generating preview.png

---
+++ scripts/generatePreview.ts
@@ -0,0 +1,46 @@
+import path from 'path';
+import fs from 'fs';
+import { createCanvas, Image } from 'canvas';
+
+const MAX_COLUMNS = 8;
+const EMOJI_SIZE = 256;
+const SPACE_PER_EMOJI = 300;
+const SPACING = SPACE_PER_EMOJI - EMOJI_SIZE;
+
+const pngDirectory = path.resolve('.', 'png');
+
+if (!fs.existsSync(pngDirectory)) {
+  throw new Error('png directory does not exist');
+}
+
+const filenames = fs.readdirSync(pngDirectory);
+
+const rows = Math.ceil(filenames.length / MAX_COLUMNS);
+const columns = rows === 1 ? filenames.length : MAX_COLUMNS;
+
+const height = rows * SPACE_PER_EMOJI;
+const width = columns * SPACE_PER_EMOJI;
+
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = '#cfb0eb';
+ctx.fillRect(0, 0, width, height);
+
+filenames.forEach((filename, index) => {
+  const filePath = path.resolve(pngDirectory, filename);
+
+  const x = (index % MAX_COLUMNS) * SPACE_PER_EMOJI + SPACING / 2;
+  const y = Math.floor(index / MAX_COLUMNS) * SPACE_PER_EMOJI + SPACING / 2;
+
+  const fileData = fs.readFileSync(filePath);
+  const image = new Image();
+  image.src = fileData;
+  image.width = EMOJI_SIZE;
+  ctx.drawImage(image, x, y);
+});
+
+const output = canvas.toBuffer();
+const outPath = path.resolve('.', 'preview.png');
+
+fs.writeFileSync(outPath, output);
